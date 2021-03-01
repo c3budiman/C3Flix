77d16fa8f163fa6b296bc59ac135526d71202637
Cecep Budiman <c3budiman@gmail.com>
2021-03-01 21:41:44 +0700

    linter again

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
 import promise from 'redux-promise-middleware';
-import { createLogger } from 'redux-logger';
+// import { createLogger } from 'redux-logger';
 import rootReducer from './reducers/all'; 
 
 const store = createStore(rootReducer, {}, applyMiddleware(
